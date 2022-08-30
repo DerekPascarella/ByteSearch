d9f23d7024379c7881555e34acf5ce1e720b2ca2
Derek Pascarella <contact.pascarella@gmail.com>
2022-08-30 09:15:19 -0400

    Updated to version 1.1

--- byte_search.pl
+++ byte_search.pl
@@ -94,7 +94,8 @@ elsif($source_type eq "file")
 	($bytes_source = uc(&read_bytes($source, (stat $source)[7]))) =~ s/\s+//g;
 }
 
-# Remove trailing double-quotation mark from target path if present from a known issue with PowerShell.
+# Remove trailing double-quotation mark from target path if present from a known issue with
+# PowerShell.
 if(substr($target, -1) eq "\"" || substr($target, -1) eq "\\")
 {
 	$target = substr($target, 0, -1);
@@ -111,13 +112,13 @@ print "> Initiating scan process against " . scalar(@files) . " files...\n\n";
 foreach(@files)
 {
 	# Store target byte-string and convert it to uppercase and remove whitespace.
-	($bytes_target = uc(&read_bytes($_, (stat $_)[7]))) =~ s/\s+//g;
+	($bytes_target = uc(&read_bytes($_))) =~ s/\s+//g;
 
 	# Source byte-string was found in target file.
 	if($bytes_target =~ $bytes_source)
 	{
 		# Store offset of match.
-		$bytes_index = &decimal_to_hex(index($bytes_target, $bytes_source) / 2, 1);
+		$bytes_index = &decimal_to_hex(index($bytes_target, $bytes_source) / 2);
 
 		# Correct forward slash.
 		if($^O =~ "MSWin")
@@ -160,11 +161,16 @@ sub show_error
 # returning hexadecimal representation of data.
 #
 # 1st parameter - Full path of file to read.
-# 2nd parameter - Number of bytes to read.
+# 2nd parameter - Number of bytes to read (omit parameter to read entire file).
 sub read_bytes
 {
 	my $file_to_read = $_[0];
 	my $bytes_to_read = $_[1];
+
+	if($bytes_to_read eq "")
+	{
+		$bytes_to_read = (stat $file_to_read)[7];
+	}
 
 	open my $filehandle, '<:raw', "$file_to_read" or die $!;
 	read $filehandle, my $bytes, $bytes_to_read;
@@ -176,8 +182,14 @@ sub read_bytes
 # Subroutine to return hexadecimal representation of a decimal number.
 #
 # 1st parameter - Decimal number.
-# 2nd parameter - Number of bytes with which to represent hexadecimal number.
+# 2nd parameter - Number of bytes with which to represent hexadecimal number (omit parameter for no
+#                 padding).
 sub decimal_to_hex
 {
+	if($_[1] eq "")
+	{
+		$_[1] = 0;
+	}
+
 	return sprintf("%0" . $_[1] * 2 . "X", $_[0]);
 }
